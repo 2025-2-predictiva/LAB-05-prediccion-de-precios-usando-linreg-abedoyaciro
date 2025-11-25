a265be62b66d91fdba959ba4eef6412465c9a3d7
abedoyaciro <abedoyaci@unal.edu.co>
2025-11-25 00:04:17 -0500

    try1

--- homework/homework.py
+++ homework/homework.py
@@ -61,3 +61,64 @@
 # {'type': 'metrics', 'dataset': 'train', 'r2': 0.8, 'mse': 0.7, 'mad': 0.9}
 # {'type': 'metrics', 'dataset': 'test', 'r2': 0.7, 'mse': 0.6, 'mad': 0.8}
 #
+
+
+import pandas as pd
+import os
+import gzip
+import pickle
+import json
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score, mean_squared_error, median_absolute_error
+
+# Load and preprocess data
+def load_and_preprocess(path):
+    df = pd.read_csv(path, compression="zip")
+    df['Age'] = 2021 - df['Year']
+    df.drop(['Year', 'Car_Name'], axis=1, inplace=True)
+    return df.drop('Present_Price', axis=1), df['Present_Price']
+
+x_train, y_train = load_and_preprocess("files/input/train_data.csv.zip")
+x_test, y_test = load_and_preprocess("files/input/test_data.csv.zip")
+
+# Create and train pipeline
+cat_cols = ['Fuel_Type', 'Selling_type', 'Transmission']
+num_cols = ['Selling_Price', 'Driven_kms', 'Owner', 'Age']
+
+model = GridSearchCV(
+    Pipeline([
+        ('preprocessor', ColumnTransformer([
+            ('cat', OneHotEncoder(), cat_cols),
+            ('num', MinMaxScaler(), num_cols)
+        ])),
+        ('feature_selection', SelectKBest(f_regression)),
+        ('regressor', LinearRegression())
+    ]),
+    {'feature_selection__k': range(1, 12)},
+    cv=10,
+    scoring='neg_mean_absolute_error',
+    n_jobs=-1
+).fit(x_train, y_train)
+
+# Save model
+os.makedirs('files/models', exist_ok=True)
+with gzip.open('files/models/model.pkl.gz', 'wb') as f:
+    pickle.dump(model, f)
+
+# Save metrics
+os.makedirs('files/output', exist_ok=True)
+with open('files/output/metrics.json', 'w') as f:
+    for name, X, y in [('train', x_train, y_train), ('test', x_test, y_test)]:
+        pred = model.predict(X)
+        f.write(json.dumps({
+            'type': 'metrics',
+            'dataset': name,
+            'r2': float(r2_score(y, pred)),
+            'mse': float(mean_squared_error(y, pred)),
+            'mad': float(median_absolute_error(y, pred))
+        }) + '\n')
